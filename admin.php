af95ae5bd13bca6cf33abe8de9e15dcb8f89dd0a
7abhisathish <89024850+7abhisathish@users.noreply.github.com>
2021-12-02 11:41:24 +0530

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Role based login system</title>
+</head>
+
+
+<body>
+	admin page
+</body>
+</html>	
---
+++ user.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta charset="utf-8">
+<title>Role based login system</title>
+</head>
+
+
+<body>
+	user page
+</body>
+</html>	
